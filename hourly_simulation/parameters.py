fb5bd5bb172b95bc49c30428b28c9ccf792df2ca
t8875881 <razsap6@gmail.com>
2022-04-05 14:07:23 +0300

    Added parameters and basic cost function

---
+++ hourly_simulation/parameters.py
@@ -0,0 +1,24 @@
+import pandas as pd
+# todo: add units
+
+# Batteries
+BATTERY_EFFICIENCY = 0.89   # %
+BATTERY_DEPTH = 0.8         # %
+BATTERY_OPEX = 15.6         # ILS / kW / year
+BATTERY_CAPEX = 1004        # ILS per Kw
+
+# Electricity
+ELECTRICITY_COST_PATH = '../data/electricity_cost.csv'
+ELECTRICITY_COST = pd.read_csv(ELECTRICITY_COST_PATH)   # ILS per Kw
+
+# Solar Panels
+SOLAR_OPEX = 70.4       # ILS / kW / year
+SOLAR_CAPEX = 3300      # ILS per Kw
+SOLAR_PANEL_AREA_DUNAM = 1.0/1000.0     # meter^2
+SOLAR_KWH_PER_DUNAM = 1000.0/10.0       # KVH/DUNAM
+
+
+# future constants
+MAXIMUM_SELLING = 1
+BATTERY_LIFETIME = 0
+SOLAR_LIFETIME = 0
--- hourly_simulation/simulation.py
+++ hourly_simulation/simulation.py
@@ -2,6 +2,10 @@ from typing import Tuple, Callable
 
 import pandas as pd
 
+from parameters import *
+
+
+# todo: add units to parameters
 
 def get_solar_production_profile(single_solar_profile: pd.DataFrame, num_solar_panels: int) -> pd.DataFrame:
     """
@@ -12,18 +16,37 @@ def get_solar_production_profile(single_solar_profile: pd.DataFrame, num_solar_p
     pass
 
 
-def calculate_cost(electricity_usage: pd.DataFrame, battery_size: float, num_solar_panels: int) -> float:
+def calculate_cost(electricity_usage: pd.DataFrame, battery_capacity: float, num_solar_panels: int,
+                   time_span: float) -> float:
     """
 
+    :param time_span: the time of the run
     :param num_solar_panels: int number of solar panels
-    :param battery_size: float size of batteries
+    :param battery_capacity: float capacity of batteries in Kwh
     :param electricity_usage: pd.DataFrame(columns=['GasUsage', 'SolarUsage', 'StoredUsage', 'SolarStored', 'SolarLost'])
     :return: float cost of the given electricity usage
+    """
+    gas_usage = electricity_usage.loc[:, "GasUsage"]
+    gas_cost_per_hour = ELECTRICITY_COST.loc[:, "Cost ILS/Kwh"]
+    total_gas_cost = gas_usage.dot(gas_cost_per_hour)
+    total_solar_opex = num_solar_panels * SOLAR_PANEL_AREA_DUNAM * SOLAR_KWH_PER_DUNAM * SOLAR_OPEX
+    total_solar_capex = num_solar_panels * SOLAR_PANEL_AREA_DUNAM * SOLAR_KWH_PER_DUNAM * SOLAR_CAPEX / time_span
+    total_battery_opex = battery_capacity * BATTERY_OPEX
+    total_battery_capex = battery_capacity * BATTERY_CAPEX / time_span
+    return total_gas_cost + total_solar_opex + total_solar_capex + total_battery_opex + total_battery_capex
+
+
+def calculate_pollution(electricity_usage: pd.DataFrame):
+    """
+    Calculate the total pollution created by the gas in the given year.
+    :param electricity_usage: pd.DataFrame(columns=['GasUsage', 'SolarUsage', 'StoredUsage', 'SolarStored', 'SolarLost'])
+    :return: float pollution in units? # todo: define the wanted units
     """
     pass
 
 
-def simulate_use(demand: pd.DataFrame, num_solar_panels: int, battery_storage: float, strategy: Callable, year: int) -> Tuple[
+def simulate_use(demand: pd.DataFrame, num_solar_panels: int, battery_storage: float, strategy: Callable, year: int) -> \
+Tuple[
     float, float]:
     """
     simulate the usages based of demand. number of solar panels and number of panels.
