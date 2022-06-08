c99709ad52b13a73fe064cd4b8533427a1b9bf4e
t8875881 <razsap6@gmail.com>
2022-06-08 08:15:21 +0300

    Merge commit

--- hourly_simulation/strategies/__init__.py
+++ hourly_simulation/strategies/__init__.py
@@ -3,5 +3,5 @@ from hourly_simulation.strategies.selling_strategy import first_selling_strategy
 from hourly_simulation.strategies.smart_storing import smart_storing_strategy
 
 use_strategies = {"Greedy Strategy": greedy_strategy.greedy_use_strategy,
-                  "Selling Strategy": selling_strategy.first_selling_strategy,
-                  "Smart Storing Strategy": smart_storing.smart_storing_strategy }
+                  # "Smart Storing Strategy": smart_storing.smart_storing_strategy,
+                  "Selling Strategy": selling_strategy.first_selling_strategy}
